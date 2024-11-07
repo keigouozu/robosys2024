c9427022ebe36a7a0b4bf768bc8bc06eb3bea6b7
Keigo Uozu <s23c1021jf@s.chibakoudai.jp>
2024-11-07 11:43:28 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -15,8 +15,8 @@ out=$(echo あ | ./plus)
 [ "${out}" = "" ] || ng "$LINENO"
 
 out=$(echo | ./plus)
-["$?" = 1 ]       || ng "$LINENO"
-["${out}" = "" ]  || ng "$LINENO"
+[ "$?" = 1 ]       || ng "$LINENO"
+[ "${out}" = "" ]  || ng "$LINENO"
 
 [ "$res" = 0 ] && echo OK
 
